aa2795bdd22860b537c284a20e5027496cd7d155
korkost <koreikinvk@gmail.com>
2022-02-21 19:17:20 +0300

    выполнил рекомендации наставника

--- project/src/components/property-screen/property-screen.tsx
+++ project/src/components/property-screen/property-screen.tsx
@@ -5,22 +5,44 @@ function PropertyScreen(): JSX.Element {
         <div className="property__gallery-container container">
           <div className="property__gallery">
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/room.jpg" alt="Studio" />
+              <img
+                className="property__image"
+                src="img/room.jpg"
+                alt="Studio"
+              />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Studio" />
+              <img
+                className="property__image"
+                src="img/apartment-01.jpg"
+                alt="Studio"
+              />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-02.jpg" alt="Studio" />
+              <img
+                className="property__image"
+                src="img/apartment-02.jpg"
+                alt="Studio"
+              />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-03.jpg" alt="Studio" />
+              <img
+                className="property__image"
+                src="img/apartment-03.jpg"
+                alt="Studio" />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/studio-01.jpg" alt="Studio" />
+              <img
+                className="property__image"
+                src="img/studio-01.jpg"
+                alt="Studio" />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Studio" />
+              <img
+                className="property__image"
+                src="img/apartment-01.jpg"
+                alt="Studio"
+              />
             </div>
           </div>
         </div>
@@ -33,7 +55,9 @@ function PropertyScreen(): JSX.Element {
               <h1 className="property__name">
                 Beautiful &amp; luxurious studio at great location
               </h1>
-              <button className="property__bookmark-button button" type="button">
+              <button
+                className="property__bookmark-button button"
+                type="button">
                 <svg className="property__bookmark-icon" width="31" height="33">
                   <use xlinkHref="#icon-bookmark"></use>
                 </svg>
@@ -101,7 +125,13 @@ function PropertyScreen(): JSX.Element {
               <h2 className="property__host-title">Meet the host</h2>
               <div className="property__host-user user">
                 <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                  <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
+                  <img
+                    className="property__avatar user__avatar"
+                    src="img/avatar-angelina.jpg"
+                    width="74"
+                    height="74"
+                    alt="Host avatar"
+                  />
                 </div>
                 <span className="property__user-name">
                     Angelina
@@ -120,12 +150,24 @@ function PropertyScreen(): JSX.Element {
               </div>
             </div>
             <section className="property__reviews reviews">
-              <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+              <h2
+                className="reviews__title">Reviews &middot;
+                <span
+                  className="reviews__amount">
+                  1
+                </span>
+              </h2>
               <ul className="reviews__list">
                 <li className="reviews__item">
                   <div className="reviews__user user">
                     <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                      <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
+                      <img
+                        className="reviews__avatar user__avatar"
+                        src="img/avatar-max.jpg"
+                        width="54"
+                        height="54"
+                        alt="Reviews avatar"
+                      />
                     </div>
                     <span className="reviews__user-name">
                         Max
@@ -145,50 +187,144 @@ function PropertyScreen(): JSX.Element {
                   </div>
                 </li>
               </ul>
-              <form className="reviews__form form" action="#" method="post">
+              <form
+                className="reviews__form form"
+                action="#"
+                method="post"
+              >
                 <label className="reviews__label form__label" htmlFor="review">Your review</label>
                 <div className="reviews__rating-form form__rating">
-                  <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-                  <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                    <svg className="form__star-image" width="37" height="33">
+                  <input
+                    className="form__rating-input visually-hidden"
+                    name="rating"
+                    value="5"
+                    id="5-stars"
+                    type="radio"
+                  />
+                  <label
+                    htmlFor="5-stars"
+                    className="reviews__rating-label form__rating-label"
+                    title="perfect"
+                  >
+                    <svg
+                      className="form__star-image"
+                      width="37"
+                      height="33"
+                    >
                       <use xlinkHref="#icon-star"></use>
                     </svg>
                   </label>
 
-                  <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-                  <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                    <svg className="form__star-image" width="37" height="33">
+                  <input
+                    className="form__rating-input visually-hidden"
+                    name="rating"
+                    value="4"
+                    id="4-stars"
+                    type="radio"
+                  />
+                  <label
+                    htmlFor="4-stars"
+                    className="reviews__rating-label form__rating-label"
+                    title="good"
+                  >
+                    <svg
+                      className="form__star-image"
+                      width="37"
+                      height="33"
+                    >
                       <use xlinkHref="#icon-star"></use>
                     </svg>
                   </label>
 
-                  <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-                  <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                    <svg className="form__star-image" width="37" height="33">
+                  <input
+                    className="form__rating-input visually-hidden"
+                    name="rating"
+                    value="3"
+                    id="3-stars"
+                    type="radio"
+                  />
+                  <label
+                    htmlFor="3-stars"
+                    className="reviews__rating-label form__rating-label"
+                    title="not bad"
+                  >
+                    <svg
+                      className="form__star-image"
+                      width="37"
+                      height="33"
+                    >
                       <use xlinkHref="#icon-star"></use>
                     </svg>
                   </label>
 
-                  <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-                  <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                    <svg className="form__star-image" width="37" height="33">
+                  <input
+                    className="form__rating-input visually-hidden"
+                    name="rating"
+                    value="2"
+                    id="2-stars"
+                    type="radio"
+                  />
+                  <label
+                    htmlFor="2-stars"
+                    className="reviews__rating-label form__rating-label"
+                    title="badly"
+                  >
+                    <svg
+                      className="form__star-image"
+                      width="37"
+                      height="33"
+                    >
                       <use xlinkHref="#icon-star"></use>
                     </svg>
                   </label>
 
-                  <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-                  <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                    <svg className="form__star-image" width="37" height="33">
+                  <input
+                    className="form__rating-input visually-hidden"
+                    name="rating"
+                    value="1"
+                    id="1-star"
+                    type="radio"
+                  />
+                  <label
+                    htmlFor="1-star"
+                    className="reviews__rating-label form__rating-label"
+                    title="terribly"
+                  >
+                    <svg
+                      className="form__star-image"
+                      width="37"
+                      height="33"
+                    >
                       <use xlinkHref="#icon-star"></use>
                     </svg>
                   </label>
                 </div>
-                <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+                <textarea
+                  className="reviews__textarea form__textarea"
+                  id="review"
+                  name="review"
+                  placeholder="Tell how was your stay, what you like and what can be improved"
+                >
+                </textarea>
                 <div className="reviews__button-wrapper">
                   <p className="reviews__help">
-                    To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+                    To submit review please make sure to set
+                    <span
+                      className="reviews__star">rating
+                    </span>
+                    and describe your stay with at least
+                    <b
+                      className="reviews__text-amount"
+                    >
+                      50 characters
+                    </b>.
                   </p>
-                  <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+                  <button
+                    className="reviews__submit form__submit button"
+                    type="submit" disabled
+                  >
+                    Submit
+                  </button>
                 </div>
               </form>
             </section>
@@ -198,12 +334,20 @@ function PropertyScreen(): JSX.Element {
       </section>
       <div className="container">
         <section className="near-places places">
-          <h2 className="near-places__title">Other places in the neighbourhood</h2>
+          <h2 className="near-places__title">
+            Other places in the neighbourhood
+          </h2>
           <div className="near-places__list places__list">
             <article className="near-places__card place-card">
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="/">
-                  <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place" />
+                  <img
+                    className="place-card__image"
+                    src="img/room.jpg"
+                    width="260"
+                    height="200"
+                    alt="Place"
+                  />
                 </a>
               </div>
               <div className="place-card__info">
@@ -212,8 +356,15 @@ function PropertyScreen(): JSX.Element {
                     <b className="place-card__price-value">&euro;80</b>
                     <span className="place-card__price-text">&#47;&nbsp;night</span>
                   </div>
-                  <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                    <svg className="place-card__bookmark-icon" width="18" height="19">
+                  <button
+                    className="place-card__bookmark-button place-card__bookmark-button--active button"
+                    type="button"
+                  >
+                    <svg
+                      className="place-card__bookmark-icon"
+                      width="18"
+                      height="19"
+                    >
                       <use xlinkHref="#icon-bookmark"></use>
                     </svg>
                     <span className="visually-hidden">In bookmarks</span>
@@ -235,7 +386,13 @@ function PropertyScreen(): JSX.Element {
             <article className="near-places__card place-card">
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="/">
-                  <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place" />
+                  <img
+                    className="place-card__image"
+                    src="img/apartment-02.jpg"
+                    width="260"
+                    height="200"
+                    alt="Place"
+                  />
                 </a>
               </div>
               <div className="place-card__info">
@@ -245,7 +402,11 @@ function PropertyScreen(): JSX.Element {
                     <span className="place-card__price-text">&#47;&nbsp;night</span>
                   </div>
                   <button className="place-card__bookmark-button button" type="button">
-                    <svg className="place-card__bookmark-icon" width="18" height="19">
+                    <svg
+                      className="place-card__bookmark-icon"
+                      width="18"
+                      height="19"
+                    >
                       <use xlinkHref="#icon-bookmark"></use>
                     </svg>
                     <span className="visually-hidden">To bookmarks</span>
@@ -270,7 +431,13 @@ function PropertyScreen(): JSX.Element {
               </div>
               <div className="near-places__image-wrapper place-card__image-wrapper">
                 <a href="/">
-                  <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place" />
+                  <img
+                    className="place-card__image"
+                    src="img/apartment-03.jpg"
+                    width="260"
+                    height="200"
+                    alt="Place"
+                  />
                 </a>
               </div>
               <div className="place-card__info">
@@ -280,7 +447,11 @@ function PropertyScreen(): JSX.Element {
                     <span className="place-card__price-text">&#47;&nbsp;night</span>
                   </div>
                   <button className="place-card__bookmark-button button" type="button">
-                    <svg className="place-card__bookmark-icon" width="18" height="19">
+                    <svg
+                      className="place-card__bookmark-icon"
+                      width="18"
+                      height="19"
+                    >
                       <use xlinkHref="#icon-bookmark"></use>
                     </svg>
                     <span className="visually-hidden">To bookmarks</span>
