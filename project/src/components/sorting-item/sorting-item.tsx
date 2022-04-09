966ee24a0da09c042330bab4afde9804eef1319d
korkost <koreikinvk@gmail.com>
2022-04-09 20:10:07 +0300

    переустановил пакеты

--- project/src/components/sorting-item/sorting-item.tsx
+++ project/src/components/sorting-item/sorting-item.tsx
@@ -1,4 +1,3 @@
-import cn from 'classnames';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { changeSorting } from '../../store/offers-process/offers-process';
 import { getSortingType } from '../../store/offers-process/selectors';
@@ -17,7 +16,7 @@ function SortingItem({ type, toggle }:
   return (
     <li
       key={type} onClick={() => { changeSortingType(type); toggle(); }}
-      className={cn('places__option', { 'places__option--active': type === sortingType })}
+      className={`places__option ${sortingType===type&&'places__option--active'}`}
       tabIndex={0}
       data-testid="SortingItem"
     >{type}
